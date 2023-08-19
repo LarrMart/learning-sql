9d65be7461731ed5fdb1906fcca5644d9a3022ba
Guillermo Larroca Martínez <larrocaguillermo@gmail.com>
2023-08-19 13:10:03 -0300

    Creación de tabla de salario de supervisores

---
+++ ch5/create_table_supervisor_salaries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE supervisor_salaries (
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	town text,
+	county text,
+	supervisor text,start_date date,
+	salary numeric(10,2),
+	benefits numeric(10,2)
+);
